fa36d3b4d340175b309bf70b5ae2ebbc8eed8023
Springhawk27 <sajjad0mahmud@gmail.com>
2023-07-15 02:16:56 +0600

    book routes implemented

---
+++ src/app/modules/book/book.routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { BookController } from './book.controller';
+
+const router = express.Router();
+
+router.post('/', BookController.createBook);
+router.get('/:id', BookController.getSingleBook);
+router.patch('/:id', BookController.updateBook);
+router.delete('/:id', BookController.deleteBook);
+router.get('/', BookController.getAllBooks);
+
+export const BookRoutes = router;
